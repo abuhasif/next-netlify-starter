4b1495491c27a893f4a48064616c642c4a29f0c7
abuhasif <2200663@sit.singaporetech.edu.sg>
2025-02-02 04:21:18 +0800

    edit

--- pages/no.js
+++ pages/no.js
@@ -2,7 +2,11 @@ export default function No() {
     return (
       <div style={{ textAlign: 'center', padding: '50px' }}>
         <h1>Oh no! Maybe next time? 😢</h1>
+        <img 
+          src="https://media.giphy.com/media/d2lcHJTG5Tscg/giphy.gif" 
+          alt="Sad GIF" 
+          style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
+        />
       </div>
     );
-  }
-  
+}
--- pages/yes.js
+++ pages/yes.js
@@ -2,7 +2,11 @@ export default function Yes() {
     return (
       <div style={{ textAlign: 'center', padding: '50px' }}>
         <h1>Yay! You made my day! ❤️</h1>
+        <img 
+          src="https://media.giphy.com/media/l3q2K5jinAlChoCLS/giphy.gif" 
+          alt="Happy GIF" 
+          style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
+        />
       </div>
     );
-  }
-  
+}
